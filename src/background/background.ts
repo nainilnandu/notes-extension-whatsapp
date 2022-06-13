1521f07b7bc3506888c4405a4ab93304d59fe538
nainilnandu <nainilnandu@gmail.com>
2022-06-13 22:18:08 +0530

    Deleted the code of bg script as it was unnecessary

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,18 +1,1 @@
 // TODO: background script
-
-import { useContext } from "react"
-import NotesContext from "../context/NotesContext"
-// chrome.runtime.onInstalled.addListener(() => {
-//   // TODO: on installed function
-// })
-
-
-const {displayNotes} = useContext(NotesContext)
-
-chrome.runtime.onMessage.addListener(reciever);
-
-function reciever(request, sender, sendResponse){
-  console.log(request);
-  console.log("in bg")
-  displayNotes(request.name)
-}
