d0faa574e4518a7bc9082a647851bb451f75c7de
nainilnandu <nainilnandu@gmail.com>
2022-06-14 00:14:45 +0530

    Zordaar bug fix

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NotesItem from './NotesItem'
 import { useContext } from 'react'
 import NotesContext from '../context/NotesContext'
@@ -7,9 +7,13 @@ import NotesContext from '../context/NotesContext'
 
 function NotesList({name}) {
 
+    
+    const {notes} = useContext(NotesContext);
     // Fetching the notes of user whose chat is currently open
+    
     const userNotes =  localStorage.getItem(name)===null ? []: JSON.parse(localStorage.getItem(name))
-
+    
+    
     console.log("UserNotes: ",userNotes);
 
     if(!userNotes || userNotes.length===0)
@@ -19,14 +23,14 @@ function NotesList({name}) {
 
     return (
         <div>
-        {userNotes.map((item) =>(
-            <NotesItem 
-            key = {item.id} 
-            item = {item} 
-            name = {name}
-            />
-        ))}
-    </div>
+            {userNotes.map((item) =>(
+                <NotesItem 
+                key = {item.id} 
+                item = {item} 
+                name = {name}
+                />
+            ))}
+        </div>
     )
 }
 
